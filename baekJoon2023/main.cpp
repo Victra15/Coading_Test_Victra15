2d34e97f1e99dce7c8f7c0ecd63f10b09c076a6a
Victra15 <Victra15@github.com>
2024-04-11 15:28:43 +0900

    baekJoon2023 on progress

---
+++ baekJoon2023/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <string>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int N;
+	std::cin >> N;
+
+	std::stack<int> decimalStack;
+	
+	return 0;
+}
